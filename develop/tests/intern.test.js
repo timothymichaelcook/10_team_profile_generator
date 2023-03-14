8429dd32feb26855a8623a8333dc0b3d38f9b7d2
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-14 15:07:55 -0400

    finish writing test for intern object

---
+++ develop/tests/intern.test.js
@@ -0,0 +1,19 @@
+const intern = require('../lib/intern.js');
+
+test('Can set school value via argument passed', () => {
+  const testValue = 'UoR';
+  const e = new intern('test', 1, 'testEmail.test.com', testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test('The getRole function returns "school"', () => {
+  const testValue = 'intern';
+  const e = new intern('test', 1, 'testEmail.test.com', testValue);
+  expect(e.getRole()).toBe(testValue);
+});
+
+test('Can get school via getSchool function"', () => {
+  const testValue = 'UoR';
+  const e = new intern('test', 1, 'testEmail.test.com', testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
--- develop/tests/manager.test.js
+++ develop/tests/manager.test.js
@@ -1,7 +1,7 @@
 const manager = require('../lib/manager.js');
 const employee = require('../lib/employee.js');
 
-test('Can set office number via argument passed', () => {
+test('Can set office number value via argument passed', () => {
   const testValue = 100;
   const e = new manager('test', 1, 'testEmail.test.com', testValue);
   expect(e.officeNumber).toBe(testValue);
@@ -13,7 +13,7 @@ test('The getRole function returns "manager"', () => {
   expect(e.getRole()).toBe(testValue);
 });
 
-test('Can get office number via getOffice()"', () => {
+test('Can get office number via getOffice function"', () => {
   const testValue = 100;
   const e = new manager('test', 1, 'testEmail.test.com', testValue);
   expect(e.getOfficeNumber()).toBe(testValue);
