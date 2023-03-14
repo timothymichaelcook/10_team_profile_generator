8324c5f806af972c47267eca7eb9155b93cb16bf
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-14 12:55:01 -0400

    declared render variables for manager, engineer, intern in htmlRenderer file

--- develop/lib/htmlRenderer.js
+++ develop/lib/htmlRenderer.js
@@ -8,7 +8,44 @@ const templatesDir = path.resolve(__dirname, '../src');
 
 
 
+const renderManager = manager => {
+  let template = fs.readFileSync(path.resolve(templatesDir, 'manager.html'), 'utf8');
+  template = replacePlaceholders(template, 'name', intern.getName());
+  template = replacePlaceholders(template, 'role', intern.getRole());
+  template = replacePlaceholders(template, 'email', intern.getEmail());
+  template = replacePlaceholders(template, 'id', intern.getId());
+  template = replacePlaceholders(template, 'officeNumber', intern.getOfficeNumber());
+  return template;
+}
 
+const renderEngineer = engineer => {
+  let template = fs.readFileSync(path.resolve(templatesDir, 'engineer.html'), 'utf8');
+  template = replacePlaceholders(template, 'name', intern.getName());
+  template = replacePlaceholders(template, 'role', intern.getRole());
+  template = replacePlaceholders(template, 'email', intern.getEmail());
+  template = replacePlaceholders(template, 'id', intern.getId());
+  template = replacePlaceholders(template, 'github', intern.getGitHub());
+  return template;
+}
 
+const renderIntern = intern => {
+  let template = fs.readFileSync(path.resolve(templatesDir, 'intern.html'), 'utf8');
+  template = replacePlaceholders(template, 'name', intern.getName());
+  template = replacePlaceholders(template, 'role', intern.getRole());
+  template = replacePlaceholders(template, 'email', intern.getEmail());
+  template = replacePlaceholders(template, 'id', intern.getId());
+  template = replacePlaceholders(template, 'school', intern.getSchool());
+  return template;
+}
+
+const renderMain = html => {
+  const template = fs.readFileSync(path.resolve(templatesDir, 'main.html'), 'utf8');
+  return replacePlaceholders(template, 'team', html);
+}
+
+const replacePlaceholders = (template, placeholder, value) => {
+  const pattern = new RegExp('{{ ' + placeholder + '}}', 'gm');
+  return template.replace(pattern, value);
+}
 
 module.exports = render;
