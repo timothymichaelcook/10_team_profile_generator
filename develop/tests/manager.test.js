ba740843ac6606369e9da6041b65b6ae2a3fa84f
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-14 15:02:39 -0400

    finished tests for manager object

---
+++ develop/tests/manager.test.js
@@ -0,0 +1,21 @@
+const manager = require('../lib/manager.js');
+const employee = require('../lib/employee.js');
+
+test('Can set office number via argument passed', () => {
+  const testValue = 100;
+  const e = new manager('test', 1, 'testEmail.test.com', testValue);
+  expect(e.officeNumber).toBe(testValue);
+});
+
+test('The getRole function returns "manager"', () => {
+  const testValue = 'manager';
+  const e = new manager('test', 1, 'testEmail.test.com', testValue);
+  expect(e.getRole()).toBe(testValue);
+});
+
+test('Can get office number via getOffice()"', () => {
+  const testValue = 100;
+  const e = new manager('test', 1, 'testEmail.test.com', testValue);
+  expect(e.getOfficeNumber()).toBe(testValue);
+});
+
